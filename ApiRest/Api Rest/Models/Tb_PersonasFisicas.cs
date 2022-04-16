db34953db1ed9a1f9e61e9162498348adca2acf5
Ivan Guerrero Ruiz <guerreroruizivan@gmail.com>
2022-04-16 00:35:20 -0500

    wip: api creation

--- ApiRest/Api Rest/Models/Tb_PersonasFisicas.cs
+++ ApiRest/Api Rest/Models/Tb_PersonasFisicas.cs
@@ -10,23 +10,16 @@ namespace Api_Rest.Models
     public class Tb_PersonasFisicas
     {
         [Key] public int IdPersonaFisica { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
-        public string? FechaRegistro { get; set; }
+        public DateTime? FechaRegistro { get; set; }
 
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
-        public string? FechaActualizacion { get; set; }
+        public DateTime? FechaActualizacion { get; set; }
 
         public string? Nombre { get; set; }
         public string? ApellidoPaterno { get; set; }
         public string? ApellidoMaterno { get; set; }
         public string? RFC { get; set; }
 
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
-        public string? FechaNacimiento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
         public int? UsuarioAgrega { get; set; }
         public bool? Activo { get; set; }
     }
