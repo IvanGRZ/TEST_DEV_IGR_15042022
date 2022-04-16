d693f8defa30a815b0c71e681d91276c89b583d1
Ivan Guerrero Ruiz <guerreroruizivan@gmail.com>
2022-04-15 23:53:32 -0500

    wip: api creation

---
+++ ApiRest/Api Rest/Models/Tb_PersonasFisicas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Rest.Models
+{
+    public class Tb_PersonasFisicas
+    {
+        [Key] public int IdPersonaFisica { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string? FechaRegistro { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string? FechaActualizacion { get; set; }
+
+        public string? Nombre { get; set; }
+        public string? ApellidoPaterno { get; set; }
+        public string? ApellidoMaterno { get; set; }
+        public string? RFC { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string? FechaNacimiento { get; set; }
+        public int? UsuarioAgrega { get; set; }
+        public bool? Activo { get; set; }
+    }
+}
--- ApiRest/Api Rest/Program.cs
+++ ApiRest/Api Rest/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Api_Rest.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Api_RestContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Api_RestContext") ?? throw new InvalidOperationException("Connection string 'Api_RestContext' not found.")));
 
 // Add services to the container.
 
